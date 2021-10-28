1c20c9311c3c76a2418eeaaf2f704aa65582e116
Rishabh Tater <rishabhtater14@gmail.com>
2021-10-29 01:20:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import OpeningPage from './components/openingPage/OpeningPage';
+import AboutUs from './components/aboutUs/AboutUs';
+import Courses from './components/courses/Courses';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' component={OpeningPage} />
+          <Route exact path='/about' component={AboutUs} />
+          <Route exact path='/courses' component={Courses} />
+          <Redirect to='/' />
+        </Switch>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
